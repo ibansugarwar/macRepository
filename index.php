a70a616f99ec42ec65d33cd3fde9b9729cf5d554
ibansugarwar <ratency@gmail.com>
2020-08-15 14:15:35 +0900

    コミット

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <div>ヘッダー</div>
+  <?php include('./news.php'); ?>
+  <section id="news">
+    <h1>ニュース</h1>
+    <p>１ 新着ニュース　</p>
+    <p>２ 新着ニュース　</p>
+  </section>
+</body>
+</html>
